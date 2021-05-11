b83ba6528b4284fa2e4120a415f973031bdd213c
Даяна Тей <ditey@edu.hse.ru>
2021-05-11 11:54:59 +0300

    is done

--- Iterators/Task05/Program.cs
+++ Iterators/Task05/Program.cs
@@ -30,14 +30,20 @@ namespace Task05
         {
             try
             {
+                int value;
+                if (!int.TryParse(Console.ReadLine(), out value) || value <= 0)
+                    throw new ArgumentException();
                 MyDigits myDigits = new MyDigits();
+                MyDigits2 myDigits2 = new MyDigits2();
                 IEnumerator enumerator = myDigits.MyEnumerator(value);
+                IEnumerator enumerator2 = myDigits2.MyEnumerator(value);
 
-                IterateThroughEnumeratorWithoutUsingForeach(enumerator);
+                IterateThroughEnumeratorWithoutUsingForeach(enumerator, value);
                 Console.WriteLine();
-                IterateThroughEnumeratorWithoutUsingForeach(enumerator);
+                IterateThroughEnumeratorWithoutUsingForeach(enumerator2, value);
                 Console.WriteLine();
-                IterateThroughEnumeratorWithoutUsingForeach(enumerator);
+                enumerator.Reset();
+                IterateThroughEnumeratorWithoutUsingForeach(enumerator, value);
             }
             catch (ArgumentException)
             {
@@ -49,18 +55,63 @@ namespace Task05
             }
         }
 
-        static void IterateThroughEnumeratorWithoutUsingForeach(IEnumerator enumerator)
+        static void IterateThroughEnumeratorWithoutUsingForeach(IEnumerator enumerator, int value)
         {
+            int num = 0;
+            while (enumerator.MoveNext())
+            {
+                Console.Write(enumerator.Current);
+                ++num;
+                if (num != value)
+                    Console.Write(" ");
+            }
         }
     }
 
     class MyDigits : IEnumerator // НЕ МЕНЯТЬ ЭТУ СТРОКУ
     {
+        public int quant;
+        int number = 0;
+
+        public MyDigits() { }
+        public MyDigits(int val)
+        { quant = val; }
+
+        public IEnumerator MyEnumerator(int val) => new MyDigits(val);
 
         public bool MoveNext()
         {
-           
+            if (number > quant - 1) return false;
+            ++number; return true;
         }
 
+        public void Reset()
+        { number = 0; }
+
+        public object Current
+        { get => Math.Pow(number, 10); }
+    }
+
+    class MyDigits2 : IEnumerator
+    {
+        int number;
+
+        public MyDigits2() { }
+        public MyDigits2(int val)
+        { number = val + 1; }
+
+        public IEnumerator MyEnumerator(int val) => new MyDigits2(val);
+
+        public bool MoveNext()
+        {
+            if (number <= 1) return false;
+            --number; return true;
+        }
+
+        public void Reset()
+        { number = int.MaxValue; }
+
+        public object Current
+        { get => Math.Pow(number, 10); }
     }
 }
